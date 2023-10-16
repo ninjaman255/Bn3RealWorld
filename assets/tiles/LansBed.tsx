afa43ed6ffc8c5e86ad7a368108850055e1dc505
aaronc <aaronclevette@gmail.com>
2023-10-16 18:45:15 -0500

    added collisions to lans bed

--- assets/tiles/LansBed.tsx
+++ assets/tiles/LansBed.tsx
@@ -2,4 +2,11 @@
 <tileset version="1.8" tiledversion="1.8.6" name="LansBed" tilewidth="88" tileheight="80" tilecount="2" columns="1" objectalignment="bottom">
  <tileoffset x="0" y="32"/>
  <image source="LansBed.png" width="88" height="160"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="53" y="73">
+    <polygon points="0,0 32,-15 -27,-46 -46,-36 -46,-25"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
