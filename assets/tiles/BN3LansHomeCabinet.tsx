0e007f1d2ef000dd441c766f63336ff3f4e174ec
aaronc <aaronclevette@gmail.com>
2023-10-16 18:05:21 -0500

    added some collisions

--- assets/tiles/BN3LansHomeCabinet.tsx
+++ assets/tiles/BN3LansHomeCabinet.tsx
@@ -2,4 +2,11 @@
 <tileset version="1.8" tiledversion="1.8.6" name="BN3LansHomeCabinet" tilewidth="48" tileheight="80" tilecount="1" columns="1" objectalignment="top">
  <tileoffset x="0" y="16"/>
  <image source="BN3LansHomeCabinet.png" width="48" height="80"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="32" y="80">
+    <polygon points="0,-1 16,-9 -18,-26 -34,-18"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
--- assets/tiles/BN3LansHomeTable1.tsx
+++ assets/tiles/BN3LansHomeTable1.tsx
@@ -2,4 +2,11 @@
 <tileset version="1.8" tiledversion="1.8.6" name="BN3LansHomeTable1" tilewidth="50" tileheight="35" tilecount="1" columns="1" objectalignment="bottom">
  <tileoffset x="0" y="9"/>
  <image source="BN3LansHomeTable1.png" width="50" height="35"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="2" x="16" y="34">
+    <polygon points="6,-5 -10,-13 14,-25 30,-17"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
