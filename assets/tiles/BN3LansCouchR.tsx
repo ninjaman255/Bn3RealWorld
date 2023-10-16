ad9ec53139aa4b432d3a17d1bc2cf3e75bb748a8
aaronc <aaronclevette@gmail.com>
2023-10-16 18:33:16 -0500

    test
    couch test

--- assets/tiles/BN3LansCouchR.tsx
+++ assets/tiles/BN3LansCouchR.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="BN3LansCouchR" tilewidth="91" tileheight="78" tilecount="1" columns="1" objectalignment="bottom">
- <tileoffset x="0" y="39"/>
+<tileset version="1.8" tiledversion="1.8.6" name="BN3LansCouchR" tilewidth="91" tileheight="78" tilecount="1" columns="1" objectalignment="bottomright">
+ <tileoffset x="91" y="42"/>
  <image source="BN3LansCouchR.png" width="91" height="78"/>
 </tileset>
