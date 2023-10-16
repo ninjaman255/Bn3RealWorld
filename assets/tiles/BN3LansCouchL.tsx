c36344c6573ba94414c1e5f5662ccec67b246601
aaronc <aaronclevette@gmail.com>
2023-10-16 18:27:38 -0500

    testing

--- assets/tiles/BN3LansCouchL.tsx
+++ assets/tiles/BN3LansCouchL.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="BN3LansCouchL" tilewidth="75" tileheight="66" tilecount="1" columns="1" objectalignment="bottom">
- <tileoffset x="0" y="34"/>
+<tileset version="1.8" tiledversion="1.8.6" name="BN3LansCouchL" tilewidth="75" tileheight="66" tilecount="1" columns="1" objectalignment="bottomleft">
+ <tileoffset x="36" y="16"/>
  <image source="BN3LansCouchL.png" width="75" height="66"/>
 </tileset>
