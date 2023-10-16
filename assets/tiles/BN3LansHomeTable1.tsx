3fc34d9adc6ed2cf0bc3fbe7b6d3a4bcee3df790
aaronc <aaronclevette@gmail.com>
2023-10-16 18:09:53 -0500

    test

--- assets/tiles/BN3LansHomeTable1.tsx
+++ assets/tiles/BN3LansHomeTable1.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="BN3LansHomeTable1" tilewidth="50" tileheight="35" tilecount="1" columns="1" objectalignment="bottom">
- <tileoffset x="0" y="9"/>
+<tileset version="1.8" tiledversion="1.8.6" name="BN3LansHomeTable1" tilewidth="50" tileheight="35" tilecount="1" columns="1" objectalignment="bottomright">
+ <tileoffset x="14" y="17"/>
  <image source="BN3LansHomeTable1.png" width="50" height="35"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
