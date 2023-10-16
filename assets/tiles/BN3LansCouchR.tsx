c268efdb7d65f5a8f19b0bcd45be48a6221c8ce3
aaronc <aaronclevette@gmail.com>
2023-10-16 18:36:44 -0500

    more couch fixes

--- assets/tiles/BN3LansCouchR.tsx
+++ assets/tiles/BN3LansCouchR.tsx
@@ -1,5 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.6" name="BN3LansCouchR" tilewidth="91" tileheight="78" tilecount="1" columns="1" objectalignment="bottomright">
- <tileoffset x="91" y="42"/>
+ <tileoffset x="91" y="30"/>
  <image source="BN3LansCouchR.png" width="91" height="78"/>
 </tileset>
