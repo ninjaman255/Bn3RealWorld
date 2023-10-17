1b4928d5c62da3f4828be4227e5c966481ece40f
aaronc <aaronclevette@gmail.com>
2023-10-16 22:26:46 -0500

    one more try for getting the drawing offset correct

--- assets/tiles/LansBed.tsx
+++ assets/tiles/LansBed.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.6" name="LansBed" tilewidth="82" tileheight="74" tilecount="1" columns="1" objectalignment="bottomleft">
- <tileoffset x="82" y="30"/>
+ <tileoffset x="78" y="12"/>
  <image source="LansBed.png" width="82" height="74"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
