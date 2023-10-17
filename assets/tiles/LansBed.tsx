15226c66b9d016a6100ae2f81976f53cf797f0fd
aaronc <aaronclevette@gmail.com>
2023-10-16 22:21:06 -0500

    fixes to bed

--- assets/tiles/LansBed.tsx
+++ assets/tiles/LansBed.tsx
@@ -1,5 +1,12 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.6" name="LansBed" tilewidth="82" tileheight="74" tilecount="1" columns="1" objectalignment="bottomleft">
- <tileoffset x="41" y="43"/>
+ <tileoffset x="82" y="43"/>
  <image source="LansBed.png" width="82" height="74"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="49" y="74">
+    <polygon points="0,0 30,-15 -21,-40 -50,-25"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
