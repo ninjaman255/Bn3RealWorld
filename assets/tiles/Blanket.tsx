1163f446e386a4e719fa81cacd2752613598a9d4
aaronc <aaronclevette@gmail.com>
2023-10-16 21:41:20 -0500

    updates

---
+++ assets/tiles/Blanket.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="Blanket" tilewidth="58" tileheight="41" tilecount="1" columns="1" objectalignment="bottomright">
+ <image source="Blanket.png" width="58" height="41"/>
+</tileset>
