01714738cb79c4e83df1b74e1f998ac1f3a8ed14
aaronc <aaronclevette@gmail.com>
2023-10-16 22:12:13 -0500

    fixes to lans room

--- assets/tiles/LansBed.tsx
+++ assets/tiles/LansBed.tsx
@@ -1,12 +1,5 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="LansBed" tilewidth="82" tileheight="74" tilecount="2" columns="1" objectalignment="bottomleft">
- <tileoffset x="82" y="39"/>
+<tileset version="1.8" tiledversion="1.8.6" name="LansBed" tilewidth="82" tileheight="74" tilecount="1" columns="1" objectalignment="bottomleft">
+ <tileoffset x="41" y="43"/>
  <image source="LansBed.png" width="82" height="74"/>
- <tile id="0">
-  <objectgroup draworder="index" id="2">
-   <object id="1" x="53" y="73">
-    <polygon points="0,4 32,-13 -30,-44 -60,-29"/>
-   </object>
-  </objectgroup>
- </tile>
 </tileset>
---
+++ assets/tiles/LansDrawer.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="LansDrawer" tilewidth="47" tileheight="45" tilecount="1" columns="1" objectalignment="bottomright">
+ <tileoffset x="47" y="8"/>
+ <image source="LansDrawer.png" width="47" height="45"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="30" y="45">
+    <polygon points="0,0 17,-9 -15,-26 -30,-16"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
