425c687ad76acaa760eb8ebdf31ddaeaa8debf39
aaronc <aaronclevette@gmail.com>
2023-10-23 01:22:25 -0500

    chnages, -added flowered bush tsx file, -added more bushes to map

---
+++ assets/tiles/FlowerBush.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="FlowerBush" tilewidth="32" tileheight="24" tilecount="1" columns="1" objectalignment="bottom">
+ <tileoffset x="0" y="10"/>
+ <image source="FlowerBush.png" width="32" height="24"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-1" y="10" width="32" height="14">
+    <ellipse/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
