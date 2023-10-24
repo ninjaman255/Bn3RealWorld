de8f1b38bffeb1f564752009d383594c9dff18b3
aaronc <aaronclevette@gmail.com>
2023-10-24 15:18:48 -0500

    changes, -fixes to collissions/locations of props

--- assets/tiles/BN3ACDCTownSquirrel.tsx
+++ assets/tiles/BN3ACDCTownSquirrel.tsx
@@ -2,4 +2,11 @@
 <tileset version="1.8" tiledversion="1.8.6" name="BN3ACDCTownSquirrel" tilewidth="40" tileheight="39" tilecount="1" columns="1" objectalignment="bottom">
  <tileoffset x="0" y="21"/>
  <image source="BN3ACDCTownSquirrel.png" width="40" height="39"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="-34" y="31">
+    <polygon points="6,1 63,-26 104,-7 47,23"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
