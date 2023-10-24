cbd5fbdf06d17750820cde78f0118bda92947f57
aaronc <aaronclevette@gmail.com>
2023-10-24 13:10:48 -0500

    changes, -fixes to make an overhang so you appear under when  passing underneath dexs balcony

--- assets/tiles/BN3DexHomeOutside.tsx
+++ assets/tiles/BN3DexHomeOutside.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.6" name="BN3DexHomeOutside" tilewidth="139" tileheight="124" tilecount="1" columns="1" objectalignment="bottomleft">
- <tileoffset x="139" y="0"/>
+ <tileoffset x="139" y="21"/>
  <grid orientation="orthogonal" width="139" height="24"/>
  <image source="BN3DexHomeOutside.png" width="139" height="124"/>
  <tile id="0">
---
+++ assets/tiles/BN3DexHomeOutsideOverHang.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="BN3DexHomeOutsideOverHang" tilewidth="53" tileheight="38" tilecount="1" columns="1" objectalignment="bottom">
+ <tileoffset x="0" y="-40"/>
+ <image source="BN3DexHomeOutsideOverHang.png" width="53" height="38"/>
+</tileset>
