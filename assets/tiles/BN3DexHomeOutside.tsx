272383d9cc9555ddf4f59d367411374fe3c2f9c6
aaronc <aaronclevette@gmail.com>
2023-10-24 12:52:41 -0500

    changes, -gave dex house some colissions

--- assets/tiles/BN3DexHomeOutside.tsx
+++ assets/tiles/BN3DexHomeOutside.tsx
@@ -3,4 +3,11 @@
  <tileoffset x="139" y="0"/>
  <grid orientation="orthogonal" width="139" height="24"/>
  <image source="BN3DexHomeOutside.png" width="139" height="124"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="2" y="97">
+    <polygon points="-6,0 67,-36 147,4 132,12 108,0 91,8 115,20 107,24 75,8 42,24"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
