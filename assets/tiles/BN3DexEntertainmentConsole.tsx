47bd1f3c44fa866767d76122a619c4d5d5d983e6
aaronc <aaronclevette@gmail.com>
2023-10-25 12:06:30 -0500

    changes, -added colissions

--- assets/tiles/BN3DexEntertainmentConsole.tsx
+++ assets/tiles/BN3DexEntertainmentConsole.tsx
@@ -2,4 +2,11 @@
 <tileset version="1.8" tiledversion="1.8.6" name="BN3DexEntertainmentConsole" tilewidth="50" tileheight="68" tilecount="1" columns="1" objectalignment="bottomleft">
  <tileoffset x="50" y="9"/>
  <image source="BN3DexEntertainmentConsole.png" width="50" height="68"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="53" y="61">
+    <polygon points="3,-2 -14,8 -43,-6 -53,-6 -53,-14 -30,-19"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
--- assets/tiles/BN3DexsBookshelf.tsx
+++ assets/tiles/BN3DexsBookshelf.tsx
@@ -2,4 +2,11 @@
 <tileset version="1.8" tiledversion="1.8.6" name="BN3DexsBookshelf" tilewidth="44" tileheight="88" tilecount="1" columns="1" objectalignment="bottomleft">
  <tileoffset x="44" y="15"/>
  <image source="BN3DexsBookshelf.png" width="44" height="88"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="73">
+    <polygon points="-1,1 29,15 44,7 15,-7"/>
+   </object>
+  </objectgroup>
+ </tile>
 </tileset>
