4811ae8f7810b0c385f5de17b3d85fef5f6b3eb0
aaronc <aaronclevette@gmail.com>
2023-10-25 00:22:34 -0500

    changes, -hopefully fixed the drawing offset

--- assets/tiles/BN3DexTable.tsx
+++ assets/tiles/BN3DexTable.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.6" name="BN3DexTable" tilewidth="67" tileheight="45" tilecount="1" columns="1" objectalignment="bottomright">
- <tileoffset x="67" y="28"/>
+ <tileoffset x="67" y="15"/>
  <image source="BN3DexTable.png" width="67" height="45"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
