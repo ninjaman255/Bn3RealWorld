5b35f485b0350e1b0e2e2bca50aa42a6682199d6
aaronc <aaronclevette@gmail.com>
2023-10-25 00:18:14 -0500

    changes, -fixed table drawing offset and collisions

--- assets/tiles/BN3DexTable.tsx
+++ assets/tiles/BN3DexTable.tsx
@@ -1,11 +1,11 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="BN3DexTable" tilewidth="67" tileheight="45" tilecount="1" columns="1" objectalignment="bottomleft">
+<tileset version="1.8" tiledversion="1.8.6" name="BN3DexTable" tilewidth="67" tileheight="45" tilecount="1" columns="1" objectalignment="bottomright">
  <tileoffset x="67" y="28"/>
  <image source="BN3DexTable.png" width="67" height="45"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
    <object id="1" x="0" y="35">
-    <polygon points="1,-1 41,-21 73,-5 33,15"/>
+    <polygon points="-2,-3 38,-23 78,-3 38,17"/>
    </object>
   </objectgroup>
  </tile>
