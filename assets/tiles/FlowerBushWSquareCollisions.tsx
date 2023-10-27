c21623c10d57c2dc8ccec1d673838354f139935c
aaronc <aaronclevette@gmail.com>
2023-10-26 23:15:53 -0500

    changes, -added new assets, -added Bush variants with Square Collisions instead of round Collisions,

---
+++ assets/tiles/FlowerBushWSquareCollisions.tsx
@@ -0,0 +1,12 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="FlowerBushWSquareCollisions" tilewidth="32" tileheight="24" tilecount="1" columns="1" objectalignment="bottom">
+ <tileoffset x="0" y="12"/>
+ <image source="FlowerBush.png" width="32" height="24"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="0" y="15">
+    <polygon points="1,1 17,-7 33,1 17,9"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
