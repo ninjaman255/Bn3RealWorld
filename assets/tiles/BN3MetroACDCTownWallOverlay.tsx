efe2a1f825a0b7e159c50a9235437b45045093b8
aaronc <aaronclevette@gmail.com>
2023-10-28 16:29:58 -0500

    changes, -added wall overlay for metro ACDC Town

---
+++ assets/tiles/BN3MetroACDCTownWallOverlay.tsx
@@ -0,0 +1,5 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="BN3MetroACDCTownWallOverlay" tilewidth="32" tileheight="81" tilecount="1" columns="1" objectalignment="bottomleft">
+ <tileoffset x="0" y="16"/>
+ <image source="BN3MetroACDCTownWallOverlay.png" width="32" height="81"/>
+</tileset>
