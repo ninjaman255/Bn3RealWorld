a17341a64d866d7138250cb9db45c12a40094852
aaronc <aaronclevette@gmail.com>
2023-10-28 17:21:05 -0500

    changes, -fixing benches drawing offset

--- assets/tiles/BN3MetroACDCTownBenchSingle.tsx
+++ assets/tiles/BN3MetroACDCTownBenchSingle.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
 <tileset version="1.8" tiledversion="1.8.6" name="BN3MetroACDCTownBenchSingle" tilewidth="71" tileheight="51" tilecount="1" columns="1" objectalignment="bottomleft">
- <tileoffset x="71" y="13"/>
+ <tileoffset x="36" y="13"/>
  <image source="BN3MetroACDCTownBenchSingle.png" width="71" height="51"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
