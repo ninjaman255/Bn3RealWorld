ffefa20897d020a12e8180db503f729dabfb0188
aaronc <aaronclevette@gmail.com>
2023-10-28 16:21:02 -0500

    changes, -fixes to BN3MetroTicketCollectorSingularStand drawing offset

--- assets/tiles/BN3MetroTicketCollectorSingularStand.tsx
+++ assets/tiles/BN3MetroTicketCollectorSingularStand.tsx
@@ -1,5 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="BN3MetroTicketCollectorSingularStand" tilewidth="69" tileheight="51" tilecount="2" columns="2">
+<tileset version="1.8" tiledversion="1.8.6" name="BN3MetroTicketCollectorSingularStand" tilewidth="69" tileheight="51" tilecount="2" columns="2" objectalignment="bottomright">
+ <tileoffset x="69" y="5"/>
  <image source="BN3MetroTicketCollectorSingularStand.png" width="138" height="51"/>
  <tile id="1">
   <objectgroup draworder="index" id="2">
