205769a023ff8d492d5f9823ec532b27b3034e4f
aaronc <aaronclevette@gmail.com>
2023-11-05 15:06:53 -0600

    changes, - ripped and added in dex chair, - Gave chair colissions slight fixes to night map,   -May change night and day to be same map and setup a way to swap the assets easily in future,

---
+++ assets/tiles/BN3DexsChair.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="BN3DexsChair" tilewidth="21" tileheight="38" tilecount="1" columns="1" objectalignment="bottom">
+ <image source="BN3DexsChair.png" width="21" height="38"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="6" y="43">
+    <polygon points="-6,-2 2,-5.75 7,-3 13,-6 8,-9 14,-12 6,-16 -14,-6"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
