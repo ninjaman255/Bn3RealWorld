de823b02e464637185c7ab53ed9add9f8c0cbde5
aaronc <aaronclevette@gmail.com>
2023-11-05 14:10:29 -0600

    changes, -removed LansBookShelf from base image and created an object with colissions

---
+++ assets/tiles/LansBookShelf.tsx
@@ -0,0 +1,11 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.8" tiledversion="1.8.6" name="LansBookShelf" tilewidth="48" tileheight="72" tilecount="1" columns="1" objectalignment="top">
+ <image source="LansBookShelf.png" width="48" height="72"/>
+ <tile id="0">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="31" y="74">
+    <polygon points="2,0 -33,-18 -15,-27 21,-9"/>
+   </object>
+  </objectgroup>
+ </tile>
+</tileset>
