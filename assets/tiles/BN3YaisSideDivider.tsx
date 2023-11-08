e805e12aa6eece9e599f6e31ebf697bee0007ba8
aaronc <aaronclevette@gmail.com>
2023-11-08 02:17:18 -0600

    changes, -fixes to yais divider drawing offset

--- assets/tiles/BN3YaisSideDivider.tsx
+++ assets/tiles/BN3YaisSideDivider.tsx
@@ -1,6 +1,6 @@
 <?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.8" tiledversion="1.8.6" name="BN3YaisSideDivider" tilewidth="57" tileheight="102" tilecount="1" columns="1" objectalignment="bottomleft">
- <tileoffset x="55" y="25"/>
+<tileset version="1.8" tiledversion="1.8.6" name="BN3YaisSideDivider" tilewidth="57" tileheight="102" tilecount="1" columns="1" objectalignment="bottom">
+ <tileoffset x="0" y="25"/>
  <image source="BN3YaisSideDivider.png" width="57" height="102"/>
  <tile id="0">
   <objectgroup draworder="index" id="2">
